a8c99c6808ae8fad62262716e4ef56509dd024a6
warainXP <boniface.warain@gmail.com>
2026-06-09 22:48:27 +0300

    Add files via upload

---
+++ #Python Tkinter GUI for Rock Paper Sciss.py
@@ -0,0 +1,10 @@
+#Python Tkinter GUI for tic ac toe gamwe
+import tkinter as tk
+import tkinter.messagebox
+root = tk.Tk()
+root.title("TIC-TAC-TOE")
+digits=[1,2,3,4,5,6,7,8,9]
+mark=("")
+count=0
+panels = ['panel'] + [None] * 9
+root.mainloop()
